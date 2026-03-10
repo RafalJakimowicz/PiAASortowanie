d72fbd93f7c6da832484527483b8852c46763907
Rafał Jakimowicz <bobix7913@gmail.com>
2026-03-11 00:43:25 +0100

    heap sort

--- include/algorithms/heapsort.h
+++ include/algorithms/heapsort.h
@@ -5,10 +5,22 @@
 template <typename T>
 class Heap
 {
+  private:
+    std::size_t h_size;
+    typename std::vector<T>::iterator h_start;
+    typename std::vector<T>::iterator h_end;
+    void heapify(int index);
+
   public:
     Heap(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end)
     {
+        this->h_size = (end - start);
+        this->h_start = start;
+        this->h_end = end;
 
+        for(int i = this->h_size/2; i >= 1; i--){
+            this->heapify(i);
+        }
     }
     void removeMax();
     std::size_t size();
@@ -17,31 +29,62 @@ class Heap
 };
 
 template <typename T>
+void Heap<T>::heapify(int index){
+    int najwiekszy = index;
+    int lewe_dziecko_index = 2 * index;
+    int prawe_dziecko_index = 2 * index + 1;
+
+    if(lewe_dziecko_index <= this->h_size && (*this)[lewe_dziecko_index] > (*this)[najwiekszy]){
+        najwiekszy = lewe_dziecko_index;
+    }
+
+    if(prawe_dziecko_index <= this->h_size && (*this)[prawe_dziecko_index] > (*this)[najwiekszy]){
+        najwiekszy = prawe_dziecko_index;
+    }
+
+    if(najwiekszy != index){
+        T tmp = (*this)[index];
+        (*this)[index] = (*this)[najwiekszy];
+        (*this)[najwiekszy] = tmp;
+        this->heapify(najwiekszy);
+    }
+}
+
+template <typename T>
 void Heap<T>::removeMax()
 {
-    // TODO: zaimplementuj - zachowanie podobne jak w funkcji std::pop_heap - czyli aktualny korzeń ląduje w miejscu ostatniego liścia kopca
+    if (this->h_size == 0) {
+        return; 
+    }
+
+    T tmp = (*this)[1];
+    (*this)[1] = (*this)[this->h_size];
+    (*this)[this->h_size] = tmp;
+
+    this->h_size -= 1;
+
+    this->heapify(1);
 }
 
 template <typename T>
 std::size_t Heap<T>::size()
 {
-    // TODO: zaimplementuj
-    return 0;
+    return this->h_size;
 }
 
 template <typename T>
 bool Heap<T>::empty()
 {
-    // TODO: zaimplementuj
-    return false;
+    if(this->h_size > 0){
+        return false;
+    }
+    return true;
 }
 
 template <typename T>
 T& Heap<T>::operator[](int index)
 {
-    // TODO: zaimplementuj - wyjątkowo indeksowanie jest od 1
-    static T t;
-    return t;
+    return *(this->h_start + index - 1);
 }
 
 
@@ -53,6 +96,13 @@ class HeapSort
 public:
     void sort(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end)
     {
+        if (start == end) return;
+
+        Heap<T> kopiec(start, end);
+        while (kopiec.size() > 1)
+        {
+            kopiec.removeMax();
+        }
     };
 };
 
