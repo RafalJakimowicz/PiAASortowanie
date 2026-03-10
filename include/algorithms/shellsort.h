ed3e20ba2c5bd6114b3945d4bc469a684ef3400e
Rafaljakimowicz <bobix7913@gmail.com>
2026-03-10 10:39:33 +0100

    shell sort

--- include/algorithms/shellsort.h
+++ include/algorithms/shellsort.h
@@ -10,6 +10,18 @@ class ShellSort
 public:
     void sort(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end)
     {
+        for (int przerwa = (end-start) / 2; przerwa >= 1; przerwa /= 2){
+            for (int i = przerwa; i < (end - start); i++){
+               T key = *(start + i);
+               typename std::vector<T>::iterator j = start + i;
+
+               while (j - start >= przerwa && *(j-przerwa) > key){
+                    *j = *(j - przerwa);
+                    j-=przerwa;
+               }
+               *j=key;
+            }
+        }
     };
 };
 #endif //SORTING_ALGORITHMS_SHELLSORT_H
