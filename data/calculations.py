d3504dac14952a4fe1bb2a5541fdc0b7ac8b391d
Rafaljakimowicz <bobix7913@gmail.com>
2026-03-12 00:33:27 +0100

    python calculations

--- data/calculations.py
+++ data/calculations.py
@@ -1,7 +1,45 @@
 import numpy as np
 
-merge_lines = []
+# ma zwrocic juz policzone srednie itp z pomiarow
+# w formacie csv
+# {nazwa pliku},{n.elementow},{%.posortowanych},{sredni czas w ms},{odchylenie standardowe},
 
-with open("mergesort.csv", "r") as merge_file:
-    merge_lines = merge_file.readlines
+linie_pliku = []
+obliczenia = []
 
+with open("mergesort.csv", "r") as plik:
+    linie_pliku = plik.readlines()
+
+surowe_dane = []
+for linia in linie_pliku:
+    pola = linia.strip("\n").split(',')
+    #[n.elementow, %posortowanych, czas]
+    surowe_dane.append([pola[1], pola[2], pola[3]])
+
+
+obrobione_dane = []
+czasy_dla_setki = []
+licznik = 0
+dane_opisowe = [] # Przechowa [n.elementow, %posortowanych]
+
+for jeden_rekord in surowe_dane:
+    czas = float(jeden_rekord[2])
+    
+    if licznik == 0:
+        dane_opisowe = [jeden_rekord[0], jeden_rekord[1]]
+        
+    czasy_dla_setki.append(czas)
+    licznik += 1
+    
+    if licznik == 100:
+        srednia = float(np.mean(czasy_dla_setki))
+        odchylenie = float(np.std(czasy_dla_setki))
+        
+        gotowy_wiersz = [dane_opisowe[0], dane_opisowe[1], srednia, odchylenie]
+        obrobione_dane.append(gotowy_wiersz)
+        
+        czasy_dla_setki = []
+        licznik = 0
+
+for o in obrobione_dane:
+    print(o)
